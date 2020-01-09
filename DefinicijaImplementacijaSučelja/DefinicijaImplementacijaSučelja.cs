0b42d6e5816d7e24c2ba25d04c2fb6462b03427c
Paško Berišić <paberisi@vsite.hr>
2020-01-09 19:56:48 +0100

    Resolve DeficinacijaImplementacijeSucelja

--- DefinicijaImplementacijaSučelja/DefinicijaImplementacijaSučelja.cs
+++ DefinicijaImplementacijaSučelja/DefinicijaImplementacijaSučelja.cs
@@ -17,11 +17,18 @@ namespace Vsite.CSharp.Sučelja
     public delegate void MojDelegat(object sender, EventArgs e);
 
 
-    // TODO:020 Napišite kod klase Implementacija tako da implementira sučelje IMojeSučelje. 
-    // TODO:021 Implementirajte metodu GenerirajMojDogađaj tako da ona generira događaj MojDogađaj
-    // TODO:022 Implementirajte kod za Svojstvo
-    class Implementacija
+    class Implementacija : IMojeSučelje
     {
+        public string Svojstvo { get => svojstvo; set => svojstvo = value; }
+
+        public event MojDelegat MojDogađaj;
+
+        public void GenerirajMojDogađaj()
+        {
+            MojDogađaj?.Invoke(this, EventArgs.Empty);
+        }
+
+        private string svojstvo;
     }
 
     class DefinicijaImplementacijaSučelja
@@ -29,6 +36,7 @@ namespace Vsite.CSharp.Sučelja
         // metode kao argument ne primaju konkretne tipove nego sučelja! 
         static void IspišiSvojstvo(IMojeSučelje ms)
         {
+
             ms.Svojstvo = "Dobar dan";
             Console.WriteLine(ms.Svojstvo);
         }
@@ -46,8 +54,7 @@ namespace Vsite.CSharp.Sučelja
 
         static void Main(string[] args)
         {
-            // TODO:023 Inicijalizirajte objekt ms instancom objekta Implementacija i provjerite što će se ispisati izvođenjem koda
-            IMojeSučelje ms = null;
+            IMojeSučelje ms = new Implementacija();
 
             IspišiSvojstvo(ms);
             DigniDogađaj(ms);
@@ -56,6 +63,5 @@ namespace Vsite.CSharp.Sučelja
             Console.ReadKey(true);
         }
 
-        // TODO:024 Pokrenuti i provjeriti testove (2 testa u grupi "DefinicijaImplementacijaSučelja" moraju proći)
     }
 }
