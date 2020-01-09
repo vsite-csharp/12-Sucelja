eb4fac29d45be86bcf64568dddc0299f8239d810
Marko Boroš <Marko.Boros@vsite.hr>
2020-01-09 20:24:04 +0100

    Resolve explicit cast and virtual members

--- VirtualniČlanovi/VirtualniČlanovi.cs
+++ VirtualniČlanovi/VirtualniČlanovi.cs
@@ -10,16 +10,30 @@ namespace Vsite.CSharp.Sučelja
             string VirtualnaMetoda();
         }
 
-        // TODO:030 Definirati klasu Bazna tako da implementira sučelje ISučelje. Metoda NevirtualnaMetoda neka vraća "Bazna.NevirtualnaMetoda", a VirtualnaMetoda neka vraća "Bazna.VirtualnaMetoda".
-        public class Bazna
+        public class Bazna : ISučelje
         {
+            public string NevirtualnaMetoda()
+            {
+                return "Bazna.NevirtualnaMetoda";
+            }
+
+            public virtual string VirtualnaMetoda()
+            {
+                return "Bazna.VirtualnaMetoda";
+            }
         }
 
 
-        // TODO:031 Definirati klasu Izvedena da je izvedena iz klase Bazna te u klasi Izvedena nadglasati (override) metode iz ISučelja. Metoda NevirtualnaMetoda neka vraća "Izvedena.NevirtualnaMetoda", a VirtualnaMetoda neka vraća "Izvedena.VirtualnaMetoda".
-        // TODO:032 Provjeriti poruke o pogreškama ili upozorenja prevoditelja.
-        public class Izvedena
+        public class Izvedena:Bazna
         {
+            public new string NevirtualnaMetoda()
+            {
+                return "Izvedena.NevirtualnaMetoda";
+            }
+            public override string VirtualnaMetoda()
+            {
+                return "Izvedena.VirtualnaMetoda";
+            }
         }
 
         public static void PozoviNevirtualnuMetodu(ISučelje sučelje)
@@ -34,13 +48,11 @@ namespace Vsite.CSharp.Sučelja
 
         static void Main(string[] args)
         {
-            // TODO:033 Inicijalizirati objekt instancom klase Bazna te provjeriti ispis
-            ISučelje s1 = null;
+            ISučelje s1 = new Bazna();
             PozoviNevirtualnuMetodu(s1);
             PozoviVirtualnuMetodu(s1);
 
-            // TODO:034 Inicijalizirati objekt instancom klase Izvedena te provjeriti ispis
-            ISučelje s2 = null;
+            ISučelje s2 = new Izvedena();
             PozoviNevirtualnuMetodu(s2);
             PozoviVirtualnuMetodu(s2);
 
@@ -48,6 +60,5 @@ namespace Vsite.CSharp.Sučelja
             Console.ReadKey(true);
         }
 
-        // TODO:035 Pokrenuti i provjeriti testove (4 testa u grupi "VirtualniČlanovi" moraju proći).
     }
 }
