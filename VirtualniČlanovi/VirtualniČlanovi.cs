f6c616247fc43a1c7d0411aa6a7b07cc58b43141
Krunoslav Bučar <krbuki@gmail.com>
2020-01-09 20:14:04 +0100

    Resolve virtual Method

--- VirtualniČlanovi/VirtualniČlanovi.cs
+++ VirtualniČlanovi/VirtualniČlanovi.cs
@@ -11,15 +11,34 @@ namespace Vsite.CSharp.Sučelja
         }
 
         // TODO:030 Definirati klasu Bazna tako da implementira sučelje ISučelje. Metoda NevirtualnaMetoda neka vraća "Bazna.NevirtualnaMetoda", a VirtualnaMetoda neka vraća "Bazna.VirtualnaMetoda".
-        public class Bazna
+        public class Bazna : ISučelje // dodano ISucelje
         {
+            public string NevirtualnaMetoda()//novo dodano
+            {
+                return "Bazna.NevirtualnaMetoda";
+            }
+
+            public virtual string VirtualnaMetoda()//novo dodano+doadao virtual
+            {
+                return "Bazna.VirtualnaMetoda";
+            }
         }
 
 
-        // TODO:031 Definirati klasu Izvedena da je izvedena iz klase Bazna te u klasi Izvedena nadglasati (override) metode iz ISučelja. Metoda NevirtualnaMetoda neka vraća "Izvedena.NevirtualnaMetoda", a VirtualnaMetoda neka vraća "Izvedena.VirtualnaMetoda".
+        // TODO:031 Definirati klasu Izvedena da je izvedena iz klase Bazna te u klasi Izvedena nadglasati (override) metode iz ISučelja. 
+        //Metoda NevirtualnaMetoda neka vraća "Izvedena.NevirtualnaMetoda", a VirtualnaMetoda neka vraća "Izvedena.VirtualnaMetoda".
         // TODO:032 Provjeriti poruke o pogreškama ili upozorenja prevoditelja.
-        public class Izvedena
+        public class Izvedena : Bazna//,ISučelje//dodao :Bazna ISučelje
         {
+            public new string NevirtualnaMetoda()//novo dodano
+            {
+                return "Izvedena.NevirtualnaMetoda";
+            }
+
+            public override string VirtualnaMetoda()//novo dodano
+            {
+                return "Izvedena.VirtualnaMetoda";
+            }
         }
 
         public static void PozoviNevirtualnuMetodu(ISučelje sučelje)
@@ -35,12 +54,12 @@ namespace Vsite.CSharp.Sučelja
         static void Main(string[] args)
         {
             // TODO:033 Inicijalizirati objekt instancom klase Bazna te provjeriti ispis
-            ISučelje s1 = null;
+            ISučelje s1 = new Bazna();// umjesto  null;
             PozoviNevirtualnuMetodu(s1);
             PozoviVirtualnuMetodu(s1);
 
             // TODO:034 Inicijalizirati objekt instancom klase Izvedena te provjeriti ispis
-            ISučelje s2 = null;
+            ISučelje s2 = new Izvedena();// null;
             PozoviNevirtualnuMetodu(s2);
             PozoviVirtualnuMetodu(s2);
 
