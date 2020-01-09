089be0708907bea1a14bd8f3de561a2778634817
Fran Safranec <Fran@DESKTOP-QN7J9VS>
2020-01-09 20:45:25 +0100

    resolve type safe interfaces

--- ImplementacijaIstoimenihČlanovaIzRazličitihSučelja/ImplementacijaIstoimenihČlanovaIzRazličitihSučelja.cs
+++ ImplementacijaIstoimenihČlanovaIzRazličitihSučelja/ImplementacijaIstoimenihČlanovaIzRazličitihSučelja.cs
@@ -14,13 +14,6 @@ namespace Vsite.CSharp.Sučelja
             void Skoči();
         }
 
-        // TODO:050 U implementaciju metode Skoči u klasi Desetobojac napisati naredbu koja će ispisati "Hop". U metodi Main pozvati tu metodu, pokrenuti program i provjeriti ispis.
-
-        // TODO:051 Definirati da klasa Desetobojac implementira sučelje ISkokUvis. U implementaciji metode tog sučelja treba ispisati "Hop u vis".
-        // TODO:052 U metodi Main dodati naredbu koja će pozvati metodu Skoči iz sučelja ISkokUVis. Pokrenuti program i provjeriti ispise.
-
-        // TODO:053 Definirati da klasa Desetobojac implementira sučelje ISkokUDalj.U implementaciji metode tog sučelja treba ispisati "Hop u dalj".
-        // TODO:054 U metodi Main dodati naredbu koja će pozvati metodu Skoči iz sučelja ISkokUDalj. Pokrenuti program i provjeriti ispise.
         public class Desetobojac : ISkokUvis,ISkokUdalj
         {
             public void Skoči()
@@ -38,7 +31,6 @@ namespace Vsite.CSharp.Sučelja
             }
         }
 
-        // TODO:055 Pokrenuti i provjeriti testove (3 testa u grupi "ImplementacijeIstoimenihČlanovaRazličitihSučelja" moraju proći).
 
         static void Main(string[] args)
         {
