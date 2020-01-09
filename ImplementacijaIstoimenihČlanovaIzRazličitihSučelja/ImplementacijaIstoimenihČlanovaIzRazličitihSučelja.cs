c536ddc6d18e61d0dc941060edfc713f450463e6
db <darij.b@gmail.com>
2020-01-09 20:32:14 +0100

    Resolve  Imp Razlicitih Sucelja

--- ImplementacijaIstoimenihČlanovaIzRazličitihSučelja/ImplementacijaIstoimenihČlanovaIzRazličitihSučelja.cs
+++ ImplementacijaIstoimenihČlanovaIzRazličitihSučelja/ImplementacijaIstoimenihČlanovaIzRazličitihSučelja.cs
@@ -14,25 +14,30 @@ namespace Vsite.CSharp.Sučelja
             void Skoči();
         }
 
-        // TODO:050 U implementaciju metode Skoči u klasi Desetobojac napisati naredbu koja će ispisati "Hop". U metodi Main pozvati tu metodu, pokrenuti program i provjeriti ispis.
-
-        // TODO:051 Definirati da klasa Desetobojac implementira sučelje ISkokUvis. U implementaciji metode tog sučelja treba ispisati "Hop u vis".
-        // TODO:052 U metodi Main dodati naredbu koja će pozvati metodu Skoči iz sučelja ISkokUVis. Pokrenuti program i provjeriti ispise.
-
-        // TODO:053 Definirati da klasa Desetobojac implementira sučelje ISkokUDalj.U implementaciji metode tog sučelja treba ispisati "Hop u dalj".
-        // TODO:054 U metodi Main dodati naredbu koja će pozvati metodu Skoči iz sučelja ISkokUDalj. Pokrenuti program i provjeriti ispise.
-        public class Desetobojac
+        public class Desetobojac : ISkokUvis, ISkokUdalj
         {
             public void Skoči()
             {
+                Console.WriteLine("Hop");
+            }
+
+            void ISkokUvis.Skoči() {
+                Console.WriteLine("Hop u vis");
+            }
+
+            void ISkokUdalj.Skoči()
+            {
+                Console.WriteLine("Hop u dalj");
             }
         }
 
-        // TODO:055 Pokrenuti i provjeriti testove (3 testa u grupi "ImplementacijeIstoimenihČlanovaRazličitihSučelja" moraju proći).
 
         static void Main(string[] args)
         {
             Desetobojac db = new Desetobojac();
+            db.Skoči();
+            ((ISkokUvis)db).Skoči();
+            ((ISkokUdalj)db).Skoči();
 
 
             Console.WriteLine("GOTOVO!!!");
