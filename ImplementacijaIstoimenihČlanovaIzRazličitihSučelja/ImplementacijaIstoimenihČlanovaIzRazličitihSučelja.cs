0bbab4cb8fdbb9c0c0f92eef069b28e3d5e40471
Fran Safranec <Fran@DESKTOP-QN7J9VS>
2020-01-09 20:32:17 +0100

    resolve multiple interfaces

--- ImplementacijaIstoimenihČlanovaIzRazličitihSučelja/ImplementacijaIstoimenihČlanovaIzRazličitihSučelja.cs
+++ ImplementacijaIstoimenihČlanovaIzRazličitihSučelja/ImplementacijaIstoimenihČlanovaIzRazličitihSučelja.cs
@@ -21,10 +21,20 @@ namespace Vsite.CSharp.Sučelja
 
         // TODO:053 Definirati da klasa Desetobojac implementira sučelje ISkokUDalj.U implementaciji metode tog sučelja treba ispisati "Hop u dalj".
         // TODO:054 U metodi Main dodati naredbu koja će pozvati metodu Skoči iz sučelja ISkokUDalj. Pokrenuti program i provjeriti ispise.
-        public class Desetobojac
+        public class Desetobojac : ISkokUvis,ISkokUdalj
         {
             public void Skoči()
             {
+                Console.WriteLine("Hop");
+            }
+
+            void ISkokUvis.Skoči() {
+                Console.WriteLine("Hop u vis");
+            }
+
+            void ISkokUdalj.Skoči()
+            {
+                Console.WriteLine("Hop u dalj");
             }
         }
 
@@ -33,7 +43,9 @@ namespace Vsite.CSharp.Sučelja
         static void Main(string[] args)
         {
             Desetobojac db = new Desetobojac();
-
+            db.Skoči();
+            ((ISkokUvis)db).Skoči();
+            ((ISkokUdalj)db).Skoči();
 
             Console.WriteLine("GOTOVO!!!");
             Console.ReadKey(true);
