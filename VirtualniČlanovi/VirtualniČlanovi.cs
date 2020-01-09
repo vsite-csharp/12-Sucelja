bbaa89df22f572ac5df4b34b4a00f45dc9994da1
Senad Mandžić <semandzi@vsite.hr>
2020-01-09 20:14:22 +0100

    Resolve virtual members

--- VirtualniČlanovi/VirtualniČlanovi.cs
+++ VirtualniČlanovi/VirtualniČlanovi.cs
@@ -11,15 +11,33 @@ namespace Vsite.CSharp.Sučelja
         }
 
         // TODO:030 Definirati klasu Bazna tako da implementira sučelje ISučelje. Metoda NevirtualnaMetoda neka vraća "Bazna.NevirtualnaMetoda", a VirtualnaMetoda neka vraća "Bazna.VirtualnaMetoda".
-        public class Bazna
+        public class Bazna : ISučelje
         {
+            public string NevirtualnaMetoda()
+            {
+                return "Bazna.NevirtualnaMetoda";
+            }
+
+            public virtual string VirtualnaMetoda()
+            {
+                return "Bazna.VirtualnaMetoda";
+            }
         }
 
 
         // TODO:031 Definirati klasu Izvedena da je izvedena iz klase Bazna te u klasi Izvedena nadglasati (override) metode iz ISučelja. Metoda NevirtualnaMetoda neka vraća "Izvedena.NevirtualnaMetoda", a VirtualnaMetoda neka vraća "Izvedena.VirtualnaMetoda".
         // TODO:032 Provjeriti poruke o pogreškama ili upozorenja prevoditelja.
-        public class Izvedena
+        public class Izvedena : Bazna
         {
+            public new string NevirtualnaMetoda()
+            {
+                return "Izvedena.NevirtualnaMetoda";
+            }
+
+            public override string VirtualnaMetoda()
+            {
+                return "Izvedena.VirtualnaMetoda";
+            }
         }
 
         public static void PozoviNevirtualnuMetodu(ISučelje sučelje)
