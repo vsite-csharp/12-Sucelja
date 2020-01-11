b3fb853a7765ecca2ac85542f993d6ba29cfe204
ivek81cro <ivek81cro@gmail.com>
2020-01-11 20:31:45 +0100

    Resolve Osoba sorting

--- ImplementacijaSučelja/Osoba.cs
+++ ImplementacijaSučelja/Osoba.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 
 namespace Vsite.CSharp.Sučelja
-{
-    // TODO:001 Dopuniti klasu Osoba tako da implementira sučelje IComparable<Osoba> koje će uspoređivati osobe po prezimenu, a ako osobe imaju isto prezime, onda i po imenu. Pokrenuti program i provjeriti ispis.
-    // TODO:002 Pokrenuti testove (test iz grupe "ImplementacijaSučelja" mora proći)
-    public class Osoba
+{    
+    public class Osoba : IComparable<Osoba>
     {
         public Osoba(string ime, string prezime, DateTime datumRođenja, string mjestoRođenja)
         {
@@ -12,6 +10,13 @@ namespace Vsite.CSharp.Sučelja
             Prezime = prezime;
             DatumRođenja = datumRođenja;
             MjestoRođenja = mjestoRođenja;
+        }
+        public int CompareTo(Osoba other)
+        {
+            int rezultat = Prezime.CompareTo(other.Prezime);
+            if (rezultat == 0)
+                rezultat = Ime.CompareTo(other.Ime);
+            return rezultat;
         }
 
         public override string ToString()
