1f0c932aa5fc85b4e4da63c27ebbaef7336081f4
Tomislav Pesa <topesa@vsite.hr>
2021-01-07 21:23:30 +0100

    Resolve ImplementacijaSučelja

--- ImplementacijaSučelja/Osoba.cs
+++ ImplementacijaSučelja/Osoba.cs
@@ -2,9 +2,9 @@
 
 namespace Vsite.CSharp.Sučelja
 {
-    // TODO:001 Dopuniti klasu Osoba tako da implementira sučelje IComparable<Osoba> koje će uspoređivati osobe po prezimenu, a ako osobe imaju isto prezime, onda i po imenu. Pokrenuti program i provjeriti ispis.
-    // TODO:002 Pokrenuti testove (test iz grupe "ImplementacijaSučelja" mora proći)
-    public class Osoba
+    // Dopuniti klasu Osoba tako da implementira sučelje IComparable<Osoba> koje će uspoređivati osobe po prezimenu, a ako osobe imaju isto prezime, onda i po imenu. Pokrenuti program i provjeriti ispis.
+    // Pokrenuti testove (test iz grupe "ImplementacijaSučelja" mora proći)
+    public class Osoba : IComparable<Osoba>
     {
         public Osoba(string ime, string prezime, DateTime datumRođenja, string mjestoRođenja)
         {
@@ -17,6 +17,17 @@ namespace Vsite.CSharp.Sučelja
         public override string ToString()
         {
             return string.Format($"{Ime} {Prezime}, {DatumRođenja.ToShortDateString()}, {MjestoRođenja}");
+        }
+
+        public int CompareTo(Osoba other)
+        {
+            int rezultat = string.Compare(Prezime, other.Prezime, true);
+
+            if (rezultat == 0)
+            {
+                rezultat = string.Compare(Ime, other.Ime, true);
+            }
+            return rezultat;
         }
 
         public readonly string Ime;
