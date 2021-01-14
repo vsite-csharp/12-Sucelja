3fa9a57ef7fc44fd4b7cfdcfd7ede890ad3e9497
Ivan Ceranac <ivcerana@vsite.hr>
2021-01-14 20:07:42 +0100

    resolve typsafe implementations

--- TipskiSigurneImplementacije/TipskiSigurneImplementacije.cs
+++ TipskiSigurneImplementacije/TipskiSigurneImplementacije.cs
@@ -4,14 +4,31 @@ namespace Vsite.CSharp.Sučelja
 {
     class TipskiSigurneImplementacije
     {
-        // TODO:060 Dopuniti strukturu Duljina tako da implementira sučelje IComparable i napisati kod za metodu CompareTo.
-        public struct Duljina
+        //  Dopuniti strukturu Duljina tako da implementira sučelje IComparable i napisati kod za metodu CompareTo.
+        public struct Duljina : IComparable<Duljina>, IComparable
         {
             private int duljina;
 
             public Duljina(int duljina)
             {
                 this.duljina = duljina;
+            }
+
+            public int CompareTo(object obj)
+            {
+                //throw new NotImplementedException();
+                if (obj==null)
+                    throw new ArgumentNullException();
+                
+                if (GetType() != obj.GetType())
+                    throw new ArgumentException();
+                return CompareTo((Duljina)obj);
+            }
+
+            public int CompareTo(Duljina other)
+            {
+                //throw new NotImplementedException();
+                return duljina - other.duljina;
             }
 
             public override string ToString()
@@ -22,8 +39,8 @@ namespace Vsite.CSharp.Sučelja
 
         public static void Usporedi(Duljina d1, object d2)
         {
-            // TODO:061 Varijabli 'usporedba' pridružiti rezultat poziva metode CompareTo.
-            int usporedba = 0; // d1.CompareTo(d2);
+            //  Varijabli 'usporedba' pridružiti rezultat poziva metode CompareTo.
+            int usporedba = d1.CompareTo(d2);
             if (usporedba == 0)
                 Console.WriteLine("{0} = {1}", d1, d2);
             else if (usporedba > 0)
@@ -38,15 +55,15 @@ namespace Vsite.CSharp.Sučelja
             {
                 Duljina d1 = new Duljina(2);
                 Duljina d2 = new Duljina(1);
-                // TODO:062 Dodati naredbu koja će metodi Usporedi proslijediti duljine d1 i d2 te provjeriti ispis.
+                //  Dodati naredbu koja će metodi Usporedi proslijediti duljine d1 i d2 te provjeriti ispis.
+                Usporedi(d1, d2);
+                //  Dodati naredbu koja će metodi Usporedi proslijediti d1 i string "pero". Provjeriti javlja li prevoditelj pogrešku te ako nema pogreške, pokrenuti program i provjeriti ispis.
+                Usporedi(d1, "Pero");
+                //  Promijeniti implementaciju sučelja IComparable tako da prevoditelj javi pogrešku na usporedbu strukture Duljina i stringa. Provjeriti javlja li prevoditelj pogrešku.
 
-                // TODO:063 Dodati naredbu koja će metodi Usporedi proslijediti d1 i string "pero". Provjeriti javlja li prevoditelj pogrešku te ako nema pogreške, pokrenuti program i provjeriti ispis.
+                //  Dopuniti strukturu Duljina tako da implementira i sučelje IComparable<Duljina>. Provjeriti javlja li prevoditelj pogrešku.
 
-                // TODO:064 Promijeniti implementaciju sučelja IComparable tako da prevoditelj javi pogrešku na usporedbu strukture Duljina i stringa. Provjeriti javlja li prevoditelj pogrešku.
-
-                // TODO:065 Dopuniti strukturu Duljina tako da implementira i sučelje IComparable<Duljina>. Provjeriti javlja li prevoditelj pogrešku.
-
-                // TODO:066 Pokrenuti i provjeriti testove (3 testa u grupi "TipskiSigurnaImplementacija" moraju proći).
+                //  Pokrenuti i provjeriti testove (3 testa u grupi "TipskiSigurnaImplementacija" moraju proći).
             }
             catch (Exception e)
             {
