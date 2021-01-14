ea6de9be449832365683e36e483d5182c3e28034
Ivan Čulina <72503859+ivculina@users.noreply.github.com>
2021-01-14 19:54:28 +0100

    Resolve implicit and explicit interface conversions

--- ImplicitnaEksplicitnaPretvorba/ImplicitnaEksplicitnaPretvorba.cs
+++ ImplicitnaEksplicitnaPretvorba/ImplicitnaEksplicitnaPretvorba.cs
@@ -8,28 +8,28 @@ namespace Vsite.CSharp.Sučelja
     {
         static void Main(string[] args)
         {
-            // TODO:040 Provjeriti koja sučelja implementira klasa System.Collections.Generic.Queue<T>:
+            //  Provjeriti koja sučelja implementira klasa System.Collections.Generic.Queue<T>:
             // https://docs.microsoft.com/en-us/dotnet/api/system.collections.generic.queue-1
             Queue<string> red = new Queue<string>(new string[] { "Mirko", "Slavko", "Jure" });
 
-            // TODO:041 Napisati naredbu koja će pomoću metode Queue<T>.Enqueue() dodati još jedan element u 'red' 
-
-            // TODO:042 Proslijediti objekt 'red' metodi IspišiSveElemente():
-
+            //  Napisati naredbu koja će pomoću metode Queue<T>.Enqueue() dodati još jedan element u 'red' 
+            red.Enqueue("Pero");
+            //  Proslijediti objekt 'red' metodi IspišiSveElemente():
+            IspišiSveElemente(red);
             Console.WriteLine("GOTOVO!!!");
             Console.ReadKey(true);
         }
 
         public static void IspišiSveElemente<T>(IEnumerable<T> elementi)
         {
-            // TODO:043 Napraviti eksplicitnu pretvorbu proslijeđenog argumenta u ICollection i ispisati broj elemenata.
-
+            //  Napraviti eksplicitnu pretvorbu proslijeđenog argumenta u ICollection i ispisati broj elemenata.
+            Console.WriteLine(((ICollection)elementi).Count);
             foreach (var e in elementi)
             {
                 Console.WriteLine(e);
             }
         }
 
-        // TODO:044 Pokrenuti i provjeriti testove (test u grupi "ImplicitnaEksplicitnaPretvorba" mora proći).
+        //  Pokrenuti i provjeriti testove (test u grupi "ImplicitnaEksplicitnaPretvorba" mora proći).
     }
 }
