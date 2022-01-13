d9c9aa7429f2cc8faaeae4fe2f09c5e79f9437b0
Luka Petranović <luka.petranovic@gmail.com>
2022-01-13 20:08:59 +0100

    Eksplicitna Pretvorba

--- ImplicitnaEksplicitnaPretvorba/ImplicitnaEksplicitnaPretvorba.cs
+++ ImplicitnaEksplicitnaPretvorba/ImplicitnaEksplicitnaPretvorba.cs
@@ -10,13 +10,15 @@ namespace Vsite.CSharp.Sučelja
         {
             Console.OutputEncoding = System.Text.Encoding.UTF8;
 
-            // TODO:040 Provjeriti koja sučelja implementira klasa System.Collections.Generic.Queue<T>:
+            // :040 Provjeriti koja sučelja implementira klasa System.Collections.Generic.Queue<T>:
             // https://docs.microsoft.com/en-us/dotnet/api/system.collections.generic.queue-1
             Queue<string> red = new Queue<string>(new string[] { "Mirko", "Slavko", "Jure" });
 
-            // TODO:041 Napisati naredbu koja će pomoću metode Queue<T>.Enqueue() dodati još jedan element u 'red' 
+            // :041 Napisati naredbu koja će pomoću metode Queue<T>.Enqueue() dodati još jedan element u 'red' 
+            red.Enqueue("Ivana");
 
-            // TODO:042 Proslijediti objekt 'red' metodi IspišiSveElemente():
+            // :042 Proslijediti objekt 'red' metodi IspišiSveElemente():
+            IspišiSveElemente(red);
 
             Console.WriteLine("GOTOVO!!!");
             Console.ReadKey(true);
@@ -25,6 +27,7 @@ namespace Vsite.CSharp.Sučelja
         public static void IspišiSveElemente<T>(IEnumerable<T> elementi)
         {
             // TODO:043 Napraviti eksplicitnu pretvorbu proslijeđenog argumenta u ICollection i ispisati broj elemenata.
+            Console.WriteLine(((ICollection)elementi).Count);
 
             foreach (var e in elementi)
             {
