6d01cd40c6dc0cca322dbc2a4b5f7249204027cf
Ida Midžić <idmidzic@vsite.hr>
2022-01-13 20:09:26 +0100

    Riješeno Impl.eksp. pretvorba, do DZ

--- ImplicitnaEksplicitnaPretvorba/ImplicitnaEksplicitnaPretvorba.cs
+++ ImplicitnaEksplicitnaPretvorba/ImplicitnaEksplicitnaPretvorba.cs
@@ -15,8 +15,10 @@ namespace Vsite.CSharp.Sučelja
             Queue<string> red = new Queue<string>(new string[] { "Mirko", "Slavko", "Jure" });
 
             // TODO:041 Napisati naredbu koja će pomoću metode Queue<T>.Enqueue() dodati još jedan element u 'red' 
+            red.Enqueue("Ana");
 
             // TODO:042 Proslijediti objekt 'red' metodi IspišiSveElemente():
+            IspišiSveElemente(red);
 
             Console.WriteLine("GOTOVO!!!");
             Console.ReadKey(true);
@@ -25,6 +27,7 @@ namespace Vsite.CSharp.Sučelja
         public static void IspišiSveElemente<T>(IEnumerable<T> elementi)
         {
             // TODO:043 Napraviti eksplicitnu pretvorbu proslijeđenog argumenta u ICollection i ispisati broj elemenata.
+            Console.WriteLine(((ICollection)elementi).Count);
 
             foreach (var e in elementi)
             {
