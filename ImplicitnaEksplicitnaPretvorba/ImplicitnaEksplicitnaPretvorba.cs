86340ce6a464c253b36ffe7d4cf3f9d289d988f5
Kata Pogacic <KPogacic@dspace.hr>
2022-01-13 20:08:36 +0100

    ImplicitnaExplicitCast

--- ImplicitnaEksplicitnaPretvorba/ImplicitnaEksplicitnaPretvorba.cs
+++ ImplicitnaEksplicitnaPretvorba/ImplicitnaEksplicitnaPretvorba.cs
@@ -10,13 +10,14 @@ namespace Vsite.CSharp.Sučelja
         {
             Console.OutputEncoding = System.Text.Encoding.UTF8;
 
-            // TODO:040 Provjeriti koja sučelja implementira klasa System.Collections.Generic.Queue<T>:
+            // 040 Provjeriti koja sučelja implementira klasa System.Collections.Generic.Queue<T>:
             // https://docs.microsoft.com/en-us/dotnet/api/system.collections.generic.queue-1
             Queue<string> red = new Queue<string>(new string[] { "Mirko", "Slavko", "Jure" });
 
-            // TODO:041 Napisati naredbu koja će pomoću metode Queue<T>.Enqueue() dodati još jedan element u 'red' 
-
-            // TODO:042 Proslijediti objekt 'red' metodi IspišiSveElemente():
+            //041 Napisati naredbu koja će pomoću metode Queue<T>.Enqueue() dodati još jedan element u 'red' 
+            red.Enqueue("Ana");
+            //042 Proslijediti objekt 'red' metodi IspišiSveElemente():
+            IspišiSveElemente(red);
 
             Console.WriteLine("GOTOVO!!!");
             Console.ReadKey(true);
@@ -24,7 +25,8 @@ namespace Vsite.CSharp.Sučelja
 
         public static void IspišiSveElemente<T>(IEnumerable<T> elementi)
         {
-            // TODO:043 Napraviti eksplicitnu pretvorbu proslijeđenog argumenta u ICollection i ispisati broj elemenata.
+            //043 Napraviti eksplicitnu pretvorbu proslijeđenog argumenta u ICollection i ispisati broj elemenata.
+            Console.WriteLine(((ICollection) elementi).Count);
 
             foreach (var e in elementi)
             {
@@ -32,6 +34,6 @@ namespace Vsite.CSharp.Sučelja
             }
         }
 
-        // TODO:044 Pokrenuti i provjeriti testove (test u grupi "ImplicitnaEksplicitnaPretvorba" mora proći).
+        //044 Pokrenuti i provjeriti testove (test u grupi "ImplicitnaEksplicitnaPretvorba" mora proći).
     }
 }
