6d5bb0720cfead3eb2a1cafeca0dfe8f5263fd85
Tomo Glozinic <tglozinic@dspace.hr>
2022-01-13 19:46:24 +0100

    Sucelja_1

--- ImplementacijaSučelja/ImplementacijaSučelja.cs
+++ ImplementacijaSučelja/ImplementacijaSučelja.cs
@@ -14,7 +14,7 @@ namespace Vsite.CSharp.Sučelja
 
             try
             {
-                // TODO:000 Pokrenuti program i provjeriti ispis.
+                // 000 Pokrenuti program i provjeriti ispis.
                 List<Osoba> osobe = new List<Osoba>();
                 osobe.Add(new Osoba("Ante", "Žagar", new DateTime(1975, 12, 5), "Split"));
                 osobe.Add(new Osoba("Pero", "Perić", new DateTime(1984, 7, 21), "Osijek"));
@@ -32,7 +32,7 @@ namespace Vsite.CSharp.Sučelja
             {
                 Console.WriteLine(e);
             }
- 
+
             Console.WriteLine("GOTOVO!!!");
             Console.ReadKey(true);
         }
