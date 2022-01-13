bdf51d3b144557b7a849e1b9ce626dd60498cec5
Ivan Sokolović <isokolovic1@gmail.com>
2022-01-13 19:28:49 +0100

    Resolve interface

--- ImplementacijaSučelja/Osoba.cs
+++ ImplementacijaSučelja/Osoba.cs
@@ -2,9 +2,9 @@
 
 namespace Vsite.CSharp.Sučelja
 {
-    // TODO:001 Dopuniti klasu Osoba tako da implementira sučelje IComparable<Osoba> koje će uspoređivati osobe po prezimenu, a ako osobe imaju isto prezime, onda i po imenu. Pokrenuti program i provjeriti ispis.
-    // TODO:002 Pokrenuti testove (test iz grupe "ImplementacijaSučelja" mora proći)
-    public class Osoba
+    // :001 Dopuniti klasu Osoba tako da implementira sučelje IComparable<Osoba> koje će uspoređivati osobe po prezimenu, a ako osobe imaju isto prezime, onda i po imenu. Pokrenuti program i provjeriti ispis.
+    // :002 Pokrenuti testove (test iz grupe "ImplementacijaSučelja" mora proći)
+    public class Osoba :IComparable<Osoba>
     {
         public Osoba(string ime, string prezime, DateTime datumRođenja, string mjestoRođenja)
         {
@@ -17,6 +17,17 @@ namespace Vsite.CSharp.Sučelja
         public override string ToString()
         {
             return string.Format($"{Ime} {Prezime}, {DatumRođenja.ToShortDateString()}, {MjestoRođenja}");
+        }
+
+        public int CompareTo(Osoba other)
+        {
+            var rezultat = Prezime.CompareTo(other.Prezime);
+            if(rezultat == 0)
+            {
+                rezultat = Ime.CompareTo(other.Ime);
+            }
+            
+            return rezultat;
         }
 
         public readonly string Ime;
