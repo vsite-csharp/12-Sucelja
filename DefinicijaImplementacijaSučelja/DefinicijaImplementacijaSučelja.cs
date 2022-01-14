9dc3b5ebcf18460cd4e0653fbbe2427fbb75c271
jandela <andjela.cirjak@gmail.com>
2022-01-14 14:41:05 +0100

    Resolve definition of interface implementation

--- DefinicijaImplementacijaSučelja/DefinicijaImplementacijaSučelja.cs
+++ DefinicijaImplementacijaSučelja/DefinicijaImplementacijaSučelja.cs
@@ -17,11 +17,18 @@ namespace Vsite.CSharp.Sučelja
     public delegate void MojDelegat(object sender, EventArgs e);
 
 
-    // TODO:020 Napišite kod klase Implementacija tako da implementira sučelje IMojeSučelje. 
-    // TODO:021 Implementirajte metodu GenerirajMojDogađaj tako da ona generira događaj MojDogađaj
-    // TODO:022 Implementirajte kod za Svojstvo
-    class Implementacija
+    // Napišite kod klase Implementacija tako da implementira sučelje IMojeSučelje. 
+    // Implementirajte metodu GenerirajMojDogađaj tako da ona generira događaj MojDogađaj
+    // Implementirajte kod za Svojstvo
+    class Implementacija : IMojeSučelje
     {
+        public void GenerirajMojDogađaj()
+        {
+            MojDogađaj(this, EventArgs.Empty);
+        }
+
+        public string Svojstvo { get; set; }
+        public event MojDelegat MojDogađaj;
     }
 
     class DefinicijaImplementacijaSučelja
@@ -46,8 +53,8 @@ namespace Vsite.CSharp.Sučelja
 
         static void Main(string[] args)
         {
-            // TODO:023 Inicijalizirajte objekt ms instancom objekta Implementacija i provjerite što će se ispisati izvođenjem koda
-            IMojeSučelje ms = null;
+            // Inicijalizirajte objekt ms instancom objekta Implementacija i provjerite što će se ispisati izvođenjem koda
+            IMojeSučelje ms = new Implementacija();
 
             IspišiSvojstvo(ms);
             DigniDogađaj(ms);
@@ -56,6 +63,6 @@ namespace Vsite.CSharp.Sučelja
             Console.ReadKey(true);
         }
 
-        // TODO:024 Pokrenuti i provjeriti testove (2 testa u grupi "DefinicijaImplementacijaSučelja" moraju proći)
+        // Pokrenuti i provjeriti testove (2 testa u grupi "DefinicijaImplementacijaSučelja" moraju proći)
     }
 }
