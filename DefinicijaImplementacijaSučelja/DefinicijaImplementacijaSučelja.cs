98a3b142b09301d9da19d283b91d9cf3b3c5c79c
Antonio Brkic <ABrkic@dspace.hr>
2022-01-17 11:41:18 +0100

    Resolve DZ definition implementation interface

--- DefinicijaImplementacijaSučelja/DefinicijaImplementacijaSučelja.cs
+++ DefinicijaImplementacijaSučelja/DefinicijaImplementacijaSučelja.cs
@@ -17,11 +17,21 @@ namespace Vsite.CSharp.Sučelja
     public delegate void MojDelegat(object sender, EventArgs e);
 
 
-    // TODO:020 Napišite kod klase Implementacija tako da implementira sučelje IMojeSučelje. 
-    // TODO:021 Implementirajte metodu GenerirajMojDogađaj tako da ona generira događaj MojDogađaj
-    // TODO:022 Implementirajte kod za Svojstvo
-    class Implementacija
+    // 020 Napišite kod klase Implementacija tako da implementira sučelje IMojeSučelje. 
+    // 021 Implementirajte metodu GenerirajMojDogađaj tako da ona generira događaj MojDogađaj
+    // 022 Implementirajte kod za Svojstvo
+    class Implementacija : IMojeSučelje
     {
+        public void GenerirajMojDogađaj()
+        {
+            if (MojDogađaj != null)
+            {
+                MojDogađaj.Invoke(this, EventArgs.Empty);
+            }
+        }
+
+        public string Svojstvo { get; set; }
+        public event MojDelegat MojDogađaj;
     }
 
     class DefinicijaImplementacijaSučelja
@@ -48,8 +58,8 @@ namespace Vsite.CSharp.Sučelja
         {
             Console.OutputEncoding = System.Text.Encoding.UTF8;
 
-            // TODO:023 Inicijalizirajte objekt ms instancom objekta Implementacija i provjerite što će se ispisati izvođenjem koda
-            IMojeSučelje ms = null;
+            // 023 Inicijalizirajte objekt ms instancom objekta Implementacija i provjerite što će se ispisati izvođenjem koda
+            IMojeSučelje ms = new Implementacija();
 
             IspišiSvojstvo(ms);
             DigniDogađaj(ms);
@@ -58,6 +68,6 @@ namespace Vsite.CSharp.Sučelja
             Console.ReadKey(true);
         }
 
-        // TODO:024 Pokrenuti i provjeriti testove (2 testa u grupi "DefinicijaImplementacijaSučelja" moraju proći)
+        // 024 Pokrenuti i provjeriti testove (2 testa u grupi "DefinicijaImplementacijaSučelja" moraju proći)
     }
 }
