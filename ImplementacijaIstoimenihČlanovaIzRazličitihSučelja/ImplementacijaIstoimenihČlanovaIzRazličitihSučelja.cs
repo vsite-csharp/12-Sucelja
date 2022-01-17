af570ffb0546d86eda36296451816881fe58e240
Pavle Pavicic <ppavicic@dspace.hr>
2022-01-17 11:57:31 +0100

    Removed TODO's.

--- ImplementacijaIstoimenihČlanovaIzRazličitihSučelja/ImplementacijaIstoimenihČlanovaIzRazličitihSučelja.cs
+++ ImplementacijaIstoimenihČlanovaIzRazličitihSučelja/ImplementacijaIstoimenihČlanovaIzRazličitihSučelja.cs
@@ -16,11 +16,11 @@ namespace Vsite.CSharp.Sučelja
 
         // U implementaciju metode Skoči u klasi Desetobojac napisati naredbu koja će ispisati "Hop". U metodi Main pozvati tu metodu, pokrenuti program i provjeriti ispis.
 
-        // TODO:051 Definirati da klasa Desetobojac implementira sučelje ISkokUvis. U implementaciji metode tog sučelja treba ispisati "Hop u vis".
-        // TODO:052 U metodi Main dodati naredbu koja će pozvati metodu Skoči iz sučelja ISkokUVis. Pokrenuti program i provjeriti ispise.
+        // Definirati da klasa Desetobojac implementira sučelje ISkokUvis. U implementaciji metode tog sučelja treba ispisati "Hop u vis".
+        // U metodi Main dodati naredbu koja će pozvati metodu Skoči iz sučelja ISkokUVis. Pokrenuti program i provjeriti ispise.
 
-        // TODO:053 Definirati da klasa Desetobojac implementira sučelje ISkokUDalj.U implementaciji metode tog sučelja treba ispisati "Hop u dalj".
-        // TODO:054 U metodi Main dodati naredbu koja će pozvati metodu Skoči iz sučelja ISkokUDalj. Pokrenuti program i provjeriti ispise.
+        // Definirati da klasa Desetobojac implementira sučelje ISkokUDalj.U implementaciji metode tog sučelja treba ispisati "Hop u dalj".
+        // U metodi Main dodati naredbu koja će pozvati metodu Skoči iz sučelja ISkokUDalj. Pokrenuti program i provjeriti ispise.
         public class Desetobojac : ISkokUvis, ISkokUdalj
         {
             public void Skoči()
@@ -39,7 +39,7 @@ namespace Vsite.CSharp.Sučelja
             }
         }
 
-        // TODO:055 Pokrenuti i provjeriti testove (3 testa u grupi "ImplementacijeIstoimenihČlanovaRazličitihSučelja" moraju proći).
+        // Pokrenuti i provjeriti testove (3 testa u grupi "ImplementacijeIstoimenihČlanovaRazličitihSučelja" moraju proći).
 
         static void Main(string[] args)
         {
