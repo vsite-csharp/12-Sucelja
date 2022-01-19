9a6400e488bde50eb4cbd4e08d2e66b313db063b
Pavle Pavicic <ppavicic@dspace.hr>
2022-01-19 09:50:51 +0100

    Solved all TODO:02*.

--- DefinicijaImplementacijaSučelja/DefinicijaImplementacijaSučelja.cs
+++ DefinicijaImplementacijaSučelja/DefinicijaImplementacijaSučelja.cs
@@ -17,18 +17,18 @@ namespace Vsite.CSharp.Sučelja
     public delegate void MojDelegat(object sender, EventArgs e);
 
 
-    // TODO:020 Napišite kod klase Implementacija tako da implementira sučelje IMojeSučelje. 
+    // Napišite kod klase Implementacija tako da implementira sučelje IMojeSučelje. 
     // TODO:021 Implementirajte metodu GenerirajMojDogađaj tako da ona generira događaj MojDogađaj
     // TODO:022 Implementirajte kod za Svojstvo
     class Implementacija : IMojeSučelje
     {
-        public string Svojstvo { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Svojstvo { get; set; }
 
         public event MojDelegat MojDogađaj;
 
         public void GenerirajMojDogađaj()
         {
-            throw new NotImplementedException();
+            MojDogađaj?.Invoke(this, EventArgs.Empty);
         }
     }
 
@@ -56,8 +56,8 @@ namespace Vsite.CSharp.Sučelja
         {
             Console.OutputEncoding = System.Text.Encoding.UTF8;
 
-            // TODO:023 Inicijalizirajte objekt ms instancom objekta Implementacija i provjerite što će se ispisati izvođenjem koda
-            IMojeSučelje ms = null;
+            // Inicijalizirajte objekt ms instancom objekta Implementacija i provjerite što će se ispisati izvođenjem koda
+            IMojeSučelje ms = new Implementacija();
 
             IspišiSvojstvo(ms);
             DigniDogađaj(ms);
@@ -66,6 +66,6 @@ namespace Vsite.CSharp.Sučelja
             Console.ReadKey(true);
         }
 
-        // TODO:024 Pokrenuti i provjeriti testove (2 testa u grupi "DefinicijaImplementacijaSučelja" moraju proći)
+        // Pokrenuti i provjeriti testove (2 testa u grupi "DefinicijaImplementacijaSučelja" moraju proći)
     }
 }
