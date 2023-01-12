0995f0be61eef30989377fb8d04d10280eedf3b3
Fran Štambuk <fran.stambuk@gmail.com>
2023-01-12 20:33:59 +0100

    Resolved

--- PodrazumijevanaImplementacijaSučelja/PodrazumijevanaImplementacijaSučelja.cs
+++ PodrazumijevanaImplementacijaSučelja/PodrazumijevanaImplementacijaSučelja.cs
@@ -6,13 +6,16 @@
         {
             void Hodaj();
         }
-        // TODO:060 U IPlivajući dodati implementaciju metode Plivaj tako da ispiše "On bi i plivo!".
+        // U IPlivajući dodati implementaciju metode Plivaj tako da ispiše "On bi i plivo!".
         public interface IPlivajući
         {
-            void Plivaj();
+            void Plivaj()
+            {
+                Console.WriteLine("On bi i plivao");
+            }
         }
-        // TODO:061 Deklarirati da klasa osoba implementira sučelje IPlivajući.
-        public class Osoba : IHodajući
+        //  Deklarirati da klasa osoba implementira sučelje IPlivajući.
+        public class Osoba : IHodajući, IPlivajući
         {
             public Osoba(string ime)
             {
@@ -33,8 +36,8 @@
 
             var osoba = new Osoba("Slaviša");
             osoba.Hodaj();
-            // TODO:062 Pozvati na osobi metodu Plivaj. Što treba napraviti da bi se ona doista pozvala.
-
+            //  Pozvati na osobi metodu Plivaj. Što treba napraviti da bi se ona doista pozvala.
+            ((IPlivajući)osoba).Plivaj();
             Console.WriteLine("\nGOTOVO!!!");
         }
     }
