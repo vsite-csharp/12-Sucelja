1dbbb40eca3a66c9f8b17cf98a3d020f9cbe6c04
Nedim <neomero@vsite.hr>
2023-01-12 20:40:54 +0100

    resolve implementation of explicit conversion

--- ImplicitnaEksplicitnaPretvorba/ImplicitnaEksplicitnaPretvorba.cs
+++ ImplicitnaEksplicitnaPretvorba/ImplicitnaEksplicitnaPretvorba.cs
@@ -6,7 +6,7 @@ namespace Vsite.CSharp.Sučelja
 {
     class ImplicitnaEksplicitnaPretvorba
     {
-        // TODO:040 Pogledati definicije sučelja IHodajući i IPlivajući te definiciju klase Osoba.
+        // :040 Pogledati definicije sučelja IHodajući i IPlivajući te definiciju klase Osoba.
         public interface IHodajući
         {
             void Hodaj();
