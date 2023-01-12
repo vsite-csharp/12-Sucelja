b380b7ea289d8f008dd9e61add229a52dee7a343
Nikolina Govorušić <nina.govorusic@gmail.com>
2023-01-12 19:45:12 +0100

    Add additional sotring criteria

--- SmisaoSučelja/SmisaoSučelja.cs
+++ SmisaoSučelja/SmisaoSučelja.cs
@@ -16,27 +16,37 @@ namespace Vsite.CSharp.Sučelja
             osobe.Sort();
         }
 
+        class SortirajPoImenu : IComparer<Osoba>
+        {
+            public int Compare(Osoba? x, Osoba? y)
+            {
+                return string.Compare(x?.Ime, y?.Ime);
+            }
+        }
+
         public static void SortiranoPoImenu(List<Osoba> osobe)
         {
-            // TODO:010 Koristeći preopterećenu inačicu metode List<T>.Sort(IComparer<T>) abecedno sortirati osobe prema njihovim imenima.
+            // 010 Koristeći preopterećenu inačicu metode List<T>.Sort(IComparer<T>) abecedno sortirati osobe prema njihovim imenima.
             // https://docs.microsoft.com/en-us/dotnet/api/system.collections.generic.icomparer-1
+            osobe.Sort(new SortirajPoImenu());
 
         }
 
         public static void SortiranoPoDatumuRođenja(List<Osoba> osobe)
         {
-            // TODO:011 Koristeći preopterećenu inačicu metode List<T>.Sort(Comparison<T>) sortirati osobe prema njihovim datumima rođenja.
+            // 011 Koristeći preopterećenu inačicu metode List<T>.Sort(Comparison<T>) sortirati osobe prema njihovim datumima rođenja.
             // https://docs.microsoft.com/en-us/dotnet/api/system.comparison-1
+            osobe.Sort((o1,o2)=>DateTime.Compare(o1.DatumRođenja,o2.DatumRođenja));
 
         }
 
         public static void SortiranoPoMjestuRođenja(List<Osoba> osobe)
         {
             // TODO:012 Koristeći preopterećenu inačicu metode List<T>.Sort(Comparison<T>) osobe sortirati prema njihovim mjestima rođenja.
-
+            //DZ!!!
         }
 
-        // TODO:013 Pokrenuti program i provjeriti ispise.
+        // 013 Pokrenuti program i provjeriti ispise.
 
         // TODO:014 Pokrenuti i provjeriti testove (3 testa u grupi "SmisaoSučelja" moraju proći)
 
