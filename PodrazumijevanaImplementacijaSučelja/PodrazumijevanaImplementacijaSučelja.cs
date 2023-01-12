acf33dbcb0db1480a1b2390dc4414e465ba508ac
Nikolina Govorušić <nina.govorusic@gmail.com>
2023-01-12 20:34:18 +0100

    Resolve default interface implementation

--- PodrazumijevanaImplementacijaSučelja/PodrazumijevanaImplementacijaSučelja.cs
+++ PodrazumijevanaImplementacijaSučelja/PodrazumijevanaImplementacijaSučelja.cs
@@ -6,13 +6,16 @@
         {
             void Hodaj();
         }
-        // TODO:060 U IPlivajući dodati implementaciju metode Plivaj tako da ispiše "On bi i plivo!".
+        // 060 U IPlivajući dodati implementaciju metode Plivaj tako da ispiše "On bi i plivo!".
         public interface IPlivajući
         {
-            void Plivaj();
+            void Plivaj()
+            {
+                Console.WriteLine("On bi i plivao!");
+            }
         }
-        // TODO:061 Deklarirati da klasa osoba implementira sučelje IPlivajući.
-        public class Osoba : IHodajući
+        // 061 Deklarirati da klasa osoba implementira sučelje IPlivajući.
+        public class Osoba : IHodajući, IPlivajući
         {
             public Osoba(string ime)
             {
@@ -33,6 +36,7 @@
 
             var osoba = new Osoba("Slaviša");
             osoba.Hodaj();
+           ((IPlivajući)osoba).Plivaj();
             // TODO:062 Pozvati na osobi metodu Plivaj. Što treba napraviti da bi se ona doista pozvala.
 
             Console.WriteLine("\nGOTOVO!!!");
