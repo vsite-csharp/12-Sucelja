f3f2bc2ca5343118c224ada0032e60ade3db8063
Fran Štambuk <fran.stambuk@gmail.com>
2023-01-12 19:45:30 +0100

    Add aditional sorting criteria.

--- ImplementacijaSučelja/Osoba.cs
+++ ImplementacijaSučelja/Osoba.cs
@@ -2,8 +2,8 @@
 
 namespace Vsite.CSharp.Sučelja
 {
-    // TODO:001 Dopuniti klasu Osoba tako da implementira sučelje IComparable<Osoba> koje će uspoređivati osobe po prezimenu, a ako osobe imaju isto prezime, onda i po imenu. Pokrenuti program i provjeriti ispis.
-    // TODO:002 Pokrenuti testove (test iz grupe "ImplementacijaSučelja" mora proći)
+    //  Dopuniti klasu Osoba tako da implementira sučelje IComparable<Osoba> koje će uspoređivati osobe po prezimenu, a ako osobe imaju isto prezime, onda i po imenu. Pokrenuti program i provjeriti ispis.
+    //  Pokrenuti testove (test iz grupe "ImplementacijaSučelja" mora proći)
     public class Osoba : IComparable<Osoba>
     {
         public Osoba(string ime, string prezime, DateTime datumRođenja, string mjestoRođenja)
--- SmisaoSučelja/SmisaoSučelja.cs
+++ SmisaoSučelja/SmisaoSučelja.cs
@@ -16,18 +16,25 @@ namespace Vsite.CSharp.Sučelja
             osobe.Sort();
         }
 
+        class SortirajPoImenu : IComparer<Osoba>
+        {
+            public int Compare(Osoba? x, Osoba? y)
+            {
+                return string.Compare(x?.Ime, y?.Ime);
+            }
+        }
         public static void SortiranoPoImenu(List<Osoba> osobe)
         {
-            // TODO:010 Koristeći preopterećenu inačicu metode List<T>.Sort(IComparer<T>) abecedno sortirati osobe prema njihovim imenima.
+            //  Koristeći preopterećenu inačicu metode List<T>.Sort(IComparer<T>) abecedno sortirati osobe prema njihovim imenima.
             // https://docs.microsoft.com/en-us/dotnet/api/system.collections.generic.icomparer-1
-
+            osobe.Sort();
         }
 
         public static void SortiranoPoDatumuRođenja(List<Osoba> osobe)
         {
-            // TODO:011 Koristeći preopterećenu inačicu metode List<T>.Sort(Comparison<T>) sortirati osobe prema njihovim datumima rođenja.
+            //Koristeći preopterećenu inačicu metode List<T>.Sort(Comparison<T>) sortirati osobe prema njihovim datumima rođenja.
             // https://docs.microsoft.com/en-us/dotnet/api/system.comparison-1
-
+            osobe.Sort((o1,o2)=>DateTime.Compare(o1.DatumRođenja,o2.DatumRođenja));
         }
 
         public static void SortiranoPoMjestuRođenja(List<Osoba> osobe)
