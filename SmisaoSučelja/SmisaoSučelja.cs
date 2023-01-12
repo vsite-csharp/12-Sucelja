138b0407b9a2dbc4f59c1236d5f15eb99e185d93
Nataša Štrangar <nastrang@vsite.hr>
2023-01-12 19:45:15 +0100

    Add aditional sorting criteria

--- SmisaoSučelja/SmisaoSučelja.cs
+++ SmisaoSučelja/SmisaoSučelja.cs
@@ -16,18 +16,26 @@ namespace Vsite.CSharp.Sučelja
             osobe.Sort();
         }
 
+        class SortirajPoImenu : IComparer<Osoba>
+        {
+            public int Compare(Osoba? x, Osoba? y)
+            {
+                return string.Compare(x?.Ime, y?.Ime);
+            }
+        }
+
         public static void SortiranoPoImenu(List<Osoba> osobe)
         {
-            // TODO:010 Koristeći preopterećenu inačicu metode List<T>.Sort(IComparer<T>) abecedno sortirati osobe prema njihovim imenima.
+            // :010 Koristeći preopterećenu inačicu metode List<T>.Sort(IComparer<T>) abecedno sortirati osobe prema njihovim imenima.
             // https://docs.microsoft.com/en-us/dotnet/api/system.collections.generic.icomparer-1
-
+            osobe.Sort(new SortirajPoImenu());
         }
 
         public static void SortiranoPoDatumuRođenja(List<Osoba> osobe)
         {
-            // TODO:011 Koristeći preopterećenu inačicu metode List<T>.Sort(Comparison<T>) sortirati osobe prema njihovim datumima rođenja.
+            // :011 Koristeći preopterećenu inačicu metode List<T>.Sort(Comparison<T>) sortirati osobe prema njihovim datumima rođenja.
             // https://docs.microsoft.com/en-us/dotnet/api/system.comparison-1
-
+            osobe.Sort((o1, o2) => DateTime.Compare(o1.DatumRođenja, o2.DatumRođenja));
         }
 
         public static void SortiranoPoMjestuRođenja(List<Osoba> osobe)
@@ -36,9 +44,9 @@ namespace Vsite.CSharp.Sučelja
 
         }
 
-        // TODO:013 Pokrenuti program i provjeriti ispise.
+        // :013 Pokrenuti program i provjeriti ispise.
 
-        // TODO:014 Pokrenuti i provjeriti testove (3 testa u grupi "SmisaoSučelja" moraju proći)
+        // :014 Pokrenuti i provjeriti testove (3 testa u grupi "SmisaoSučelja" moraju proći)
 
         static void Main(string[] args)
         {
