c9f882a2c8102e391fce5423d60366c3246baac0
Ivan Davidović <hedead421@gmail.com>
2024-01-18 20:20:43 +0100

    Test Explicit interface casting

--- ImplicitnaEksplicitnaPretvorba/ImplicitnaEksplicitnaPretvorba.cs
+++ ImplicitnaEksplicitnaPretvorba/ImplicitnaEksplicitnaPretvorba.cs
@@ -11,7 +11,7 @@
         {
             void Plivaj();
         }
-        public class Osoba : IHodajući
+        public class Osoba : IHodajući , IPlivajući
         {
             public Osoba(string ime)
             {
@@ -45,8 +45,8 @@
             Console.WriteLine("\nGOTOVO!!!");
         }
 
-        // TODO:041 Pogledati definicije donje dvije metode i zaključiti što će se ispisati pokretanjem programa.
-        // TODO:042 Pokrenuti program i provjeriti ispis. Obrazložiti zašto se u drugoj metodi ne poziva metoda Plivaj.
+        // :041 Pogledati definicije donje dvije metode i zaključiti što će se ispisati pokretanjem programa.
+        // :042 Pokrenuti program i provjeriti ispis. Obrazložiti zašto se u drugoj metodi ne poziva metoda Plivaj.
         public static void ProšećiSve(IEnumerable<IHodajući> osobe)
         {
             foreach (var o in osobe)
